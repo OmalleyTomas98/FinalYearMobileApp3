41ec90120cca3935acf50e3dcd01e57b22f130cb
Tomas <tomas@tomass-imac-pro.home>
2020-11-27 22:38:27 +0000

    added camera path

---
+++ FinalYearProjectMobileApp/Assets/_Game/Scripts/PlayerMove.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] PathCreator path;
+    [SerializeField] EndOfPathInstruction endOfPath;
+    [SerializeField] float speed = 3f;
+    [SerializeField] bool isMoving = true;
+
+    [Header("Debug Options")]
+    [SerializeField] float previewDistance = 0f;
+    [SerializeField] bool enableDebug;
+
+    private float distanceTravelled;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (path != null && isMoving)
+        {
+            distanceTravelled += speed * Time.deltaTime;
+            transform.position = path.path.GetPointAtDistance(distanceTravelled, endOfPath);
+            transform.rotation = path.path.GetRotationAtDistance(distanceTravelled, endOfPath);
+        }
+    }
+
+    private void OnValidate()
+    {
+        if (enableDebug)
+        {
+            transform.position = path.path.GetPointAtDistance(previewDistance, endOfPath);
+            transform.rotation = path.path.GetRotationAtDistance(previewDistance, endOfPath);
+        }
+    }
+}
