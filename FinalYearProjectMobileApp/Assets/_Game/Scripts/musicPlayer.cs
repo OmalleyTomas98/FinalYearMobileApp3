2143044b72b0769195423ca3803f3512ae6da95b
Tomas <tomas@tomass-imac-pro.home>
2020-12-03 01:05:12 +0000

    Added menu

---
+++ FinalYearProjectMobileApp/Assets/_Game/Scripts/musicPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class musicPlayer : MonoBehaviour
+{
+
+	//Loading level 1 in game
+
+  // allow music to load to next level 
+  private void Awake()
+  {
+     DontDestroyOnLoad(this.gameObject);
+  }
+
+	// called before first frame update 
+    void Start()
+    {
+        Invoke("LoadFirstLevel", 3); // invoke the method after passed seconds 
+    }
+
+  private void LoadFirstLevel1()
+  {
+  	SceneManager.LoadScene(1);
+  }
+}
